02044cd040311fe39eff1eeeb3c0bf6c358a52f3
Mikhail Zhitkov <mikhail.zhitkov@gmail.com>
2020-04-11 15:39:13 +0300

    Учебный проект: время разбивать камни

---
+++ src/components/loadMoreButtonTemplate.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const createLoadMoreButtonTemplate = () => {
+  return (
+    `<button class="load-more" type="button"> load more</button>`
+  );
+};
+
+export { createLoadMoreButtonTemplate };
